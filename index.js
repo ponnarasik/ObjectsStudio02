1ccef28edc5ed690f92db38d3e87e7ca1b0a14b6
ponnarasi <85145307+ponnarasik@users.noreply.github.com>
2021-06-23 01:47:18 +0000

    Final

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 // Code your orbitCircumference function here:
 function orbitCircumference(radius){
-  return Math.round(2*Math.PI)
+  return Math.round(2*Math.PI*radius);
 }
 
+
 // Code your missionDuration function here:
+function missionDuration(numOrbits, orbRad=2000, speed=28000){
+  let time = (numOrbits*orbitCircumference(orbRad))/speed;
+  return Math.round(time*100)/100;
+}
 
 
 // Copy/paste your selectRandomEntry function here:
+function selectRandomEntry(arr){
+  let index = Math.floor(Math.random()*arr.length);
+  return arr[index];
+}
 
 
 // Code your oxygenExpended function here:
+function oxygenExpended(astronaut, orbRad, speed){
+  let duration = missionDuration(3);
+  let oxUsed = Math.round(astronaut.o2Used(duration)*1000)/1000;
+  return `${astronaut.name} will perform the spacewalk, which will last ${duration} hours and require ${oxUsed} kg of oxygen.`;
+}
 
 
 // Candidate data & crew array.
@@ -56,4 +70,8 @@ let candidateF = {
   'astronautID':890
 };
 
+
+
 let crew = [candidateA,candidateC,candidateE];
+let walker = crew[Math.floor(Math.random()*crew.length)];
+console.log(oxygenExpended(walker))
